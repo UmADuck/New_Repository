a69645d1994f3b9169c7823bfa4f8f505a045f1f
Dmytro Korol <dmytro.korol.ir.2021@lpnu.ua>
2021-10-31 09:39:11 +0200

    modified code counting suggestion comments

--- Lab3.c
+++ Lab3.c
@@ -1,20 +1,26 @@
 #include <stdio.h>
 #include <ctype.h>
+
 char capitalize(char m);
 int main()
 {
-    int i, k[] = { 1, 2, 5, 100 };
-    char arr[] = { 'a', 'b', 'c', 'd', 'e','f' };
+    //Array to know which element of array with letters to capitalize
+    int i, NumArr[] = { 1, 2, 5, 100 };
+    char LetterArr[] = { 'a', 'b', 'c', 'd', 'e','f' };
+
     for (i = 0; i < 4; i++)
     {
-        arr[k[i]] = capitalize(arr[k[i]]);
+        //Capitalizing elements which have the same number as number of element we take from NumArr
+        LetterArr[NumArr[i]] = capitalize(LetterArr[k[i]]);
     }
-    for (i = 0; i < sizeof(arr); i++)
+
+    for (i = 0; i < sizeof(LetterArr); i++)
     {
-        printf("%c", arr[i]);
+        printf("%c", LetterArr[i]);
     }
     return 0;
 }
+
 char capitalize(char m)
 {
     m = toupper(m);
