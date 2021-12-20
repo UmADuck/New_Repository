8d1c30a17c44771c57b459b6d85e51cd6de0e51a
UmADuck <90317303+UmADuck@users.noreply.github.com>
2021-12-20 12:37:35 +0200

    Update Lab3.c

--- Lab3.c
+++ Lab3.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 #include <ctype.h>
 
-char capitalize(char m);
+char capitalize(char symbol);
 int main()
 {
     //Array to know which element of array with letters to capitalize
-    int i, NumArr[] = { 1, 2, 5, 100 };
+    int i, NumArr[] = { 1, 2, 5, 100 }, NumOfElements;
     char LetterArr[] = { 'a', 'b', 'c', 'd', 'e','f' };
 
-    for (i = 0; i < 4; i++)
+    for (i = 0; NumOfElements < 4; i++)
     {
         //Capitalizing elements which have the same number as number of element we take from NumArr
         LetterArr[NumArr[i]] = capitalize(LetterArr[k[i]]);
@@ -21,8 +21,7 @@ int main()
     return 0;
 }
 
-char capitalize(char m)
+char capitalize(char symbol)
 {
-    m = toupper(m);
-    return(m);
+    return( toupper(m));
 }
